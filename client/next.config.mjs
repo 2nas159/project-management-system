d17242a056553ed990c5e0f3d1eb7d5d5c85a5ae
2nas159 <omeraktay159@gmail.com>
2025-07-11 11:25:57 +0300

    images

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "https://pms-project-images.s3.us-east-1.amazonaws.com",
+        hostname: "pms-project-images.s3.us-east-1.amazonaws.com",
         port: "",
         pathname: "/**",
       }

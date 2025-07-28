44955d265a0393f24aa7490629f01824570a4ba5
Ömer Aktay <omeraktay159@gmail.com>
2025-07-28 21:51:07 +0300

    vercel

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,11 @@
+import serverless from "serverless-http";
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
+
 /* ROUTE IMPORTS */
 import projectRoutes from "./routes/projectRoutes";
 import taskRoutes from "./routes/taskRoutes";
@@ -26,15 +28,11 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("This is home route");
 });
-
 app.use("/projects", projectRoutes);
 app.use("/tasks", taskRoutes);
 app.use("/search", searchRoutes);
 app.use("/users", userRoutes);
 app.use("/teams", teamRoutes);
 
-/* SERVER */
-const port = Number(process.env.PORT) || 3000;
-app.listen(port, "0.0.0.0", () => {
-  console.log(`Server running on part ${port}`);
-});
+/* EXPORT SERVERLESS HANDLER */
+export const handler = serverless(app);
